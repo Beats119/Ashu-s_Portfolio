e9c2d85e3afa9ed3a07e2ccd54cb27a7c03a07b6
Beats119 <beats119119@gmail.com>
2026-09-24 22:33:50 +0530

    refactor: mirror creative scene architecture

---
+++ src/utils/audioManager.js
@@ -0,0 +1,2 @@
+let volume=.3
+export function initAudio(){} export function getMusicVolume(){return volume} export function setMusicVolume(v){volume=v} export function playSound(){} export function stopSound(){}
