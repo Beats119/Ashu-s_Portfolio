8c81d4ee168c80b8fb63d4d6b3b62a1258feae39
Beats119 <beats119119@gmail.com>
2026-09-24 22:19:15 +0530

    build: add Vite configuration for Pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins:[react()],
+  base:'/Ashu-s_Portfolio/',
+  build:{outDir:'dist',assetsDir:'assets',emptyOutDir:true}
+})
