c3d3cfc7a1c8ecaead60abf3903b7172530c1784
Beats119 <beats119119@gmail.com>
2026-09-24 22:37:47 +0530

    fix: simplify React entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-import React from 'react'
+import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import './styles.css'
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App/></React.StrictMode>
-)
+import './styles/main.scss'
+createRoot(document.getElementById('root')).render(<StrictMode><App/></StrictMode>)
