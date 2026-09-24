d46e1c1df7ff2f4c402ae18c076b6c033d10d61b
Beats119 <beats119119@gmail.com>
2026-09-24 22:50:37 +0530

    fix: complete contact room component

--- src/components/canvas/rooms/Contact/ContactRoom.jsx
+++ src/components/canvas/rooms/Contact/ContactRoom.jsx
@@ -1,2 +1,4 @@
 import RoomInterior from '../../corridor/RoomInterior'
-export default function ContactRoom(props){return <RoomInterior {...props} roomId="contact" label="LET'S CONNECT"/>
+export default function ContactRoom(props){
+  return <RoomInterior {...props} roomId="contact" label="LET'S CONNECT"/>
+}
