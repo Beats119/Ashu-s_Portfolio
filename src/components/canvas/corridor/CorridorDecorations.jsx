895d5b868a255f54d3cabff129dcf666da452ce8
Beats119 <beats119119@gmail.com>
2026-09-24 23:01:49 +0530

    fix: complete corridor decorations JSX

--- src/components/canvas/corridor/CorridorDecorations.jsx
+++ src/components/canvas/corridor/CorridorDecorations.jsx
@@ -1,2 +1,25 @@
 import {Text} from '@react-three/drei'
-export default function CorridorDecorations({zOffset=10,length=80}){return <group>{Array.from({length:12},(_,i)=><Text key={i} position={[(i%2?-1:1)*3.05,1.1,zOffset-5-i*6]} fontSize={.09} color="#898378" rotation={[0,0,i%2?.06:-.06]}>{i%2?'// TODO: EVALUATE':'// THINK / BUILD'}</Text>)}</group>
+
+export default function CorridorDecorations({zOffset=10,length=80}){
+  const items=Array.from({length:12},(_,i)=>({
+    z:zOffset-5-i*6,
+    side:i%2===0?1:-1,
+    label:i%2===0?'// THINK / BUILD':'// TODO: EVALUATE'
+  }))
+
+  return (
+    <group>
+      {items.map((item,i)=>(
+        <Text
+          key={i}
+          position={[item.side*3.02,1.05,item.z]}
+          fontSize={.09}
+          color="#898378"
+          rotation={[0,0,item.side>0?.06:-.06]}
+        >
+          {item.label}
+        </Text>
+      ))}
+    </group>
+  )
+}
